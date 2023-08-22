452e67caad09613b8e9f08c6e274450f9cb09cd6
Bruce Hsu <1033684650@qq.com>
2023-08-22 15:42:04 +0800

    delete some files

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/*
+ * @Date: 2023-08-22 15:28:27
+ * @Author: Bruce Hsu
+ * @Description: 
+ */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
